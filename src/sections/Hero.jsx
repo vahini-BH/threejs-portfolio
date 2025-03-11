05e0202d3f4919a8af8a301ed65f9693aac2e912
vahini-BH <123856923+vahini-BH@users.noreply.github.com>
2025-03-11 11:44:25 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Vahini <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Student & Building Products</p>
+        <p className="hero_tag text-gray_gradient">📍Frontend Developer</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
